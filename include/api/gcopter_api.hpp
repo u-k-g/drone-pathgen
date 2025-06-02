e7895e292e6dd040bcbbb796d38d32004cc156d3
uzair ghori <ukghori08@gmail.com>
2025-06-02 02:12:29 -0400

    ci(env): :ferris_wheel: set up environment

---
+++ include/api/gcopter_api.hpp
@@ -0,0 +1,13 @@
+class GCopterAPI {
+public:
+  // The 4 core API methods
+  void configure_map();
+  void set_endpoints();
+  void run_inference();
+  void get_visualization_data();
+
+private:
+  // Internal GCOPTER objects
+  // Internal state variables
+  // Internal helper methods
+};
