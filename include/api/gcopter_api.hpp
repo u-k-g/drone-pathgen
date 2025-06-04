8caed8bb5e4397e83d55bffa4a14923860a81b65
uzair ghori <ukghori08@gmail.com>
2025-06-03 23:20:01 -0400

    feat(voxel map): :sparkles: configure map api header

--- include/api/gcopter_api.hpp
+++ include/api/gcopter_api.hpp
@@ -10,10 +10,23 @@
 
 class GCopterAPI {
 public:
-  void configure_map();
+  /**
+   * Build a VoxelMap from raw obstacle data and apply safety dilation.
+   *
+   * @param map_size         Number of voxels along x, y, z.
+   * @param origin           World-coordinate of the grid's (0,0,0) corner.
+   * @param voxel_scale      Edge length of each voxel (in meters).
+   * @param obstacle_points  List of obstacle positions in world coords.
+   * @param dilation_radius  Number of voxels to dilate occupied cells.
+   */
+  void configure_map(const Eigen::Vector3i &map_size,
+                     const Eigen::Vector3d &origin, double voxel_scale,
+                     const std::vector<Eigen::Vector3d> &obstacle_points,
+                     int dilation_radius = 1);
   void set_endpoints();
   void run_inference();
   void get_visualization_data();
 
 private:
+  std::unique_ptr<voxel_map::VoxelMap> map_;
 };
